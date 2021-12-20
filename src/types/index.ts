da53418eaa2deeac307159bfb39938dd48ef7d5d
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-20 17:38:11 +0100

    :construction: Work in progress

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,38 @@
  * file that was distributed with this source code.
  *
  */
+
+export enum Intent {
+  GUILDS = 1,
+  GUILD_MEMBERS = 2,
+  GUILD_BANS = 4,
+  GUILD_EMOJIS_AND_STICKERS = 8,
+  GUILD_INTEGRATIONS = 16,
+  GUILD_WEBHOOKS = 32,
+  GUILD_INVITES = 64,
+  GUILD_VOICE_STATES = 128,
+  GUILD_PRESENCES = 256,
+  GUILD_MESSAGES = 512,
+  GUILD_MESSAGE_REACTIONS = 1024,
+  GUILD_MESSAGE_TYPING = 2048,
+  DIRECT_MESSAGES = 4096,
+  DIRECT_MESSAGE_REACTIONS = 8192,
+  DIRECT_MESSAGE_TYPING = 16384,
+  ALL = 32767,
+}
+
+export type ClientOptions = {
+  shardCount?: 1
+  messageCacheLifetime?: 0
+  messageSweepInterval?: 0
+  invalidRequestWarningInterval?: 0
+  intents?: 'ALL' | Exclude<keyof typeof Intent, 'ALL'>[],
+  restWsBridgeTimeout?: 5000
+  restRequestTimeout?: 15000
+  restGlobalRateLimit?: 0
+  retryLimit?: 1
+  restTimeOffset?: 500
+  restSweepInterval?: 60
+  failIfNotExists?: true
+  userAgentSuffix?: []
+}
