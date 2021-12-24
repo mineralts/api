b93a75bf34e08cf3243fb3f7ca36eaa56d4936eb
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-24 01:05:29 +0100

    :construction: Work in progress

---
+++ src/entities/guild/GuildChannelManager.ts
@@ -0,0 +1,59 @@
+import { Collection } from '@mineralts/core'
+import { ChannelOptionResolvable, ChannelResolvable, ChannelTypeResolvable, Snowflake } from '../../types'
+import Guild from './Guild'
+
+export default class GuildChannelManager {
+  public cache: Collection<Snowflake, ChannelResolvable> = new Collection()
+
+  constructor (private guild?: Guild) {
+  }
+
+  public register (channels: Collection<Snowflake, ChannelResolvable>) {
+    this.cache = channels
+    return this
+  }
+
+  public async create (channel: ChannelOptionResolvable) {
+    let payload: any
+    const request = new Request(`/guilds/${this.guild?.id}/channels`)
+    const baseChannel = {
+      name: channel.name,
+      type: ChannelTypeResolvable[channel.type],
+      permission_overwrites: channel.permissionOverwrites || [],
+      parent_id: channel.options?.parent?.id || channel.options?.parentId
+    }
+
+    if (channel.type === 'GUILD_TEXT') {
+      // payload = await request.post({
+      //   ...baseChannel,
+      //   nsfw: channel.options?.nsfw || false,
+      //   rate_limit_per_user: channel.options?.cooldown || 0,
+      //   topic: channel.options?.topic || ''
+      // })
+    }
+
+    if (channel.type === 'GUILD_VOICE') {
+      // payload = await request.post({
+      //   ...baseChannel,
+      //   user_limit: channel.options?.userLimit || 0,
+      //   bitrate: channel.options?.bitrate || 64000
+      // })
+    }
+
+    if (channel.type === 'GUILD_CATEGORY') {
+      // payload = await request.post({
+      //   ...baseChannel
+      // })
+    }
+
+    if (channel.type === 'GUILD_STAGE_VOICE') {
+      // payload = await request.post({
+      //   ...baseChannel,
+      //   user_limit: channel.options?.userLimit || 0,
+      //   bitrate: channel.options?.bitrate || 64000
+      // })
+    }
+
+    return this.cache.get(payload.id)
+  }
+}
