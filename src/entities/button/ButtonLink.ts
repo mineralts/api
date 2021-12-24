b268daeee80eaff07645cafbd33cc91f18eed230
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-24 01:02:12 +0100

    :construction: Work in progress

---
+++ src/entities/button/ButtonLink.ts
@@ -0,0 +1,43 @@
+import Emoji from '../emoji'
+import BaseButton from './BaseButton'
+
+export default class ButtonLink extends BaseButton {
+  public url?: string
+
+  constructor (
+    props?: {
+      label?: string,
+      emoji?: string | Emoji,
+      url: string
+      disabled?: boolean
+    }
+  ) {
+    if (props) super('LINK', props.label, undefined, props.disabled)
+    else super('LINK', undefined, undefined)
+
+    if (props?.emoji) {
+      this.emoji = this.parseEmoji(props.emoji) as any
+    }
+
+    if (props?.url) {
+      this.url = props.url
+    }
+  }
+
+  public setUrl (url: string) {
+    this.url = url
+    return this
+  }
+
+  public toJson () {
+    if (!this.url) {
+      // Logger.send('error', `${this.label} component has not url.`)
+      process.exit(0)
+    }
+
+    return {
+      ...super.toJson(),
+      url: this.url
+    }
+  }
+}
