229e877ea869b09611fea748a11dde2096fdad44
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-24 01:04:54 +0100

    :construction: Work in progress

---
+++ src/entities/embed/EmbedRow.ts
@@ -0,0 +1,27 @@
+import { ComponentType, MessageComponentResolvable } from '../../types'
+
+export default class EmbedRow {
+  public type: ComponentType = ComponentType.ACTION_ROW
+  public components: MessageComponentResolvable[] = new Proxy([], {
+    set: function(target: MessageComponentResolvable[], property: string | symbol, value) {
+      if (target.length > 5) {
+        // Logger.send('error', `A row can contain a maximum of 5 components, ${target.length}.`)
+        process.exit()
+      }
+      target[property] = value
+      return true
+    }
+  })
+
+  public addComponent (component: MessageComponentResolvable) {
+    this.components.push(component)
+    return this
+  }
+
+  public addComponents (components: MessageComponentResolvable[]) {
+    components.forEach((component: MessageComponentResolvable) => {
+      this.components.push(component)
+    })
+    return this
+  }
+}
