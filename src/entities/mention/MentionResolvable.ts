660b2518fbc65b1b54605f9e9d94fcc97b941d95
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-24 08:13:11 +0100

    :construction: Work in progress

---
+++ src/entities/mention/MentionResolvable.ts
@@ -0,0 +1,15 @@
+import { Snowflake } from '../../types'
+import Role from '../roles'
+import { Collection } from '@mineralts/core'
+import GuildMember from '../guild/GuildMember'
+import Channel from '../guild/channels/Channel'
+
+export class MentionResolvable {
+  constructor (
+    public isEveryone: boolean,
+    public roles: Collection<Snowflake, Role>,
+    public members: Collection<Snowflake, GuildMember>,
+    public channels: Collection<Snowflake, Channel>
+  ) {
+  }
+}
