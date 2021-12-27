2fe7ca6f5c5eea3418446e22f214310221b035ff
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-27 15:46:46 +0100

    :construction: Work in progress

--- src/entities/client/index.ts
+++ src/entities/client/index.ts
@@ -3,8 +3,11 @@ import { ClientOptions, Snowflake } from '../../types'
 import User from '../user'
 import Presence from '../presence'
 import Command from '../../command/Command'
+import { GuildManager } from '../../../index'
 
 export default class Client {
+  public guilds: GuildManager = new GuildManager()
+
   constructor (
     private container: any,
     public readonly token: string,
@@ -13,7 +16,7 @@ export default class Client {
     public readonly sessionId: string,
     public readonly presences: Presence[],
     public readonly application: { id: string, flags: number },
-    public commands: Collection<Snowflake, Command> = new Collection()
+    public commands: Collection<Snowflake, Command> = new Collection(),
   ) {
   }
 }
