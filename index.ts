fc751218dae99db9fd565dcbf814d5ac56b1c309
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-27 15:47:21 +0100

    :construction: Work in progress

--- index.ts
+++ index.ts
@@ -10,12 +10,70 @@
 
 import Client from './src/entities/client'
 import User from './src/entities/user'
-import { ClientEvents } from './src/types'
+import GuildMember from './src/entities/guild/GuildMember'
+import Guild from './src/entities/guild/Guild'
+import Presence from './src/entities/presence'
 import MessageEmbed from './src/entities/embed/MessageEmbed'
+import Role from './src/entities/roles'
+import Emoji from './src/entities/emoji'
+import GuildRoleManager from './src/entities/guild/GuildRoleManager'
+import GuildChannelManager from './src/entities/guild/GuildChannelManager'
+import GuildMemberManager from './src/entities/guild/GuildMemberManager'
+import GuildEmojiManager from './src/entities/guild/GuildEmojiManager'
+import InviteManager from './src/entities/invitation/InviteManager'
+import GuildMemberRoleManager from './src/entities/guild/GuildMemberRoleManager'
+import VoiceState from './src/entities/voice/VoiceState'
+import Activity from './src/entities/activity'
+import TextChannel from './src/entities/channels/TextChannel'
+import VoiceChannel from './src/entities/channels/VoiceChannel'
+import CategoryChannel from './src/entities/channels/CategoryChannel'
+import MessageManager from './src/entities/message/MessageManager'
+import Collection from './src/utils/Collection'
+import GuildManager from './src/entities/GuildManager'
+import Invite from './src/entities/invitation/Invite'
+import {
+  ActivityType,
+  ChannelResolvable,
+  ChannelTypeResolvable,
+  ClientEvents,
+  PresenceStatus,
+  Region,
+  Snowflake,
+  RTC_Region,
+  VideoQuality,
+} from './src/types'
 
 export {
   Client,
   User,
   ClientEvents,
-  MessageEmbed
+  MessageEmbed,
+  Snowflake,
+  GuildMember,
+  Guild,
+  Presence,
+  Role,
+  Emoji,
+  ChannelResolvable,
+  TextChannel,
+  VoiceChannel,
+  CategoryChannel,
+  Region,
+  GuildRoleManager,
+  GuildChannelManager,
+  GuildMemberManager,
+  GuildEmojiManager,
+  GuildMemberRoleManager,
+  InviteManager,
+  VoiceState,
+  PresenceStatus,
+  Activity,
+  ActivityType,
+  MessageManager,
+  ChannelTypeResolvable,
+  RTC_Region,
+  VideoQuality,
+  Collection,
+  GuildManager,
+  Invite
 }
