c5f9353fb46985588abcfab2359772fa179fe551
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-03 21:55:54 +0100

    :construction: Add logger error

--- src/entities/embed/EmbedRow.ts
+++ src/entities/embed/EmbedRow.ts
@@ -1,12 +1,14 @@
 import { ComponentType, MessageComponentResolvable } from '../../types'
+import Application from '@mineralts/application'
 
 export default class EmbedRow {
   public type: ComponentType = ComponentType.ACTION_ROW
   public components: MessageComponentResolvable[] = new Proxy([], {
     set: function(target: MessageComponentResolvable[], property: string | symbol, value) {
       if (target.length > 5) {
-        // Logger.send('error', `A row can contain a maximum of 5 components, ${target.length}.`)
-        process.exit()
+        const logger = Application.getLogger()
+        logger.error(`A row can contain a maximum of 5 components, ${target.length}.`)
+        process.exit(1)
       }
       target[property] = value
       return true
