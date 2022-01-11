42c25ed5e29dd7e3f4e38a6b8e4113e43eccc832
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-11 10:07:25 +0100

    :construction: Create global command register method

--- src/entities/client/index.ts
+++ src/entities/client/index.ts
@@ -4,6 +4,11 @@ import User from '../user'
 import Presence from '../presence'
 import Command from '../../command/Command'
 import { GuildManager } from '../../../index'
+import Application from '@mineralts/application'
+import { MineralCommand } from '@mineralts/core'
+import { CommandContext } from '@mineralts/assembler'
+import { Assembler } from '@mineralts/assembler'
+import { serializeCommand } from '../../utils'
 
 export default class Client {
   public guilds: GuildManager = new GuildManager()
@@ -18,5 +23,21 @@ export default class Client {
     public readonly application: { id: string, flags: number },
     public commands: Collection<Snowflake, Command> = new Collection(),
   ) {
+  }
+  
+  public async registerGlobalCommands (assembler: Assembler) {
+    const request = Application.createRequest()
+    const commandContainer = assembler.application.container.commands
+    const commands = commandContainer.filter((command: MineralCommand & { data: CommandContext }) => (
+      command.data.scope == 'GLOBAL'
+    ))
+
+    await Promise.all(
+      commands.map(async (command: MineralCommand & { data: CommandContext }) => {
+        await request.post(`/applications/${this.application.id}/commands`, {
+          ...serializeCommand(command.data)
+        })
+      })
+    )
   }
 }
---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+/*
+ * packages/index.ts
+ *
+ * (c) Parmantier Baptiste
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+export interface StringArgument {
+  name: string
+  description: string
+  required: boolean
+}
+
+export interface NumberArgument {
+  name: string
+  description: string
+  required: boolean
+}
