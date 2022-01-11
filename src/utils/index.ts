536195f21b34b911c021efc14fe9fdc75996c76f
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-11 10:10:14 +0100

    :construction: Upgrade color resolver

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,13 @@
-import Color from '../entities/colors'
+import { OptionType } from '../types'
+import { CommandContext } from '@mineralts/assembler'
+
 export function resolveColor(color) {
 
   if (typeof color === 'string') {
     if (color === 'RANDOM') return Math.floor(Math.random() * (0xffffff + 1))
     if (color === 'DEFAULT') return 0
-    if (color in Color) {
-      color = parseInt(color.replace('#', ''), 16)
-    }
+
+    color = parseInt(color.replace('#', ''), 16)
   }
 
   if (color < 0 || color > 0xffffff) throw new RangeError('COLOR_RANGE')
@@ -24,4 +25,24 @@ export function parseEmoji(text: string) {
   if (!text.includes(':')) return { name: text, id: null }
   const match = text.match(/<?(?:(a):)?(\w{2,32}):(\d{17,19})?>?/)
   return match && { animated: Boolean(match[1]), name: match[2], id: match[3] ?? null }
+}
+
+export function serializeCommand (command: CommandContext) {
+  return {
+    type: 1,
+    name: command.label,
+    description: command.description,
+    options: command.options.map((option) => {
+      return {
+        name: option.name,
+        description: option.description,
+        type: OptionType[option.type],
+        required: option.required,
+        choices: option.choices,
+        min_value: option.min,
+        max_value: option.max,
+        autocomplete: OptionType[option.type] !== 'CHOICE' && option.autocomplete
+      }
+    })
+  }
 }
