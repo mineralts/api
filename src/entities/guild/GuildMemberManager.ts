c114cf925e44243061562373ac1d725690f331f5
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:40:28 +0100

    :construction: Create getters

--- src/entities/guild/GuildMemberManager.ts
+++ src/entities/guild/GuildMemberManager.ts
@@ -3,10 +3,14 @@ import GuildMember from './GuildMember'
 import { Snowflake } from '../../types'
 
 export default class GuildMemberManager {
-  public cache: Collection<Snowflake, GuildMember> = new Collection()
+  private cache: Collection<Snowflake, GuildMember> = new Collection()
 
   public register (guildMembers: Collection<Snowflake, GuildMember>) {
     this.cache = guildMembers
     return this
+  }
+
+  public getCache (): Collection<Snowflake, GuildMember> {
+    return this.cache
   }
 }
