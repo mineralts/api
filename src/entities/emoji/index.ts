e12187322905b96281149aa4f49f0dc1bacac0c5
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:44:32 +0100

    :construction: Create getters

--- src/entities/emoji/index.ts
+++ src/entities/emoji/index.ts
@@ -3,12 +3,36 @@ import Role from '../roles'
 
 export default class Emoji {
   constructor (
-    public id: Snowflake,
-    public label: string,
-    public isManaged: boolean,
-    public isAvailable: boolean,
-    public isAnimated: boolean = false,
-    public roles: Role[] = [],
+    private id: Snowflake,
+    private label: string,
+    private managed: boolean,
+    private available: boolean,
+    private animated: boolean = false,
+    private roles: Role[] = [],
   ) {
+  }
+
+  public getId (): Snowflake {
+    return this.id
+  }
+
+  public getLabel (): string {
+    return this.label
+  }
+
+  public isManaged (): boolean {
+    return this.managed
+  }
+
+  public isAvailable (): boolean {
+    return this.available
+  }
+
+  public isAnimated (): boolean {
+    return this.animated
+  }
+
+  public getRoles (): Role[] {
+    return this.roles
   }
 }
