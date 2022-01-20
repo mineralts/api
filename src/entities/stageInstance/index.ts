d16cbf60bc59a227b02e5dce202b7ad2e553c7cf
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:45:16 +0100

    :construction: Create getters

--- src/entities/stageInstance/index.ts
+++ src/entities/stageInstance/index.ts
@@ -1,13 +1,42 @@
 import { Snowflake } from '../../types'
+import Guild from '../guild/Guild'
+import Application from '@mineralts/application'
+import StageChannel from '../channels/StageChannel'
 
 export default class StageInstance {
   constructor (
-    public id: Snowflake,
-    public guildId: Snowflake,
-    public channelId: Snowflake,
-    public topic: string,
-    public privacyLevel: number,
-    public isDiscoverableDisabled: boolean,
+    private id: Snowflake,
+    private guildId: Snowflake,
+    private channelId: Snowflake,
+    private topic: string,
+    private privacyLevel: number,
+    private discoverableDisabled: boolean,
   ) {
+  }
+
+  public getId (): Snowflake {
+    return this.id
+  }
+
+  public getGuild (): Guild {
+    const client = Application.getClient()
+    return client.guilds.getCache().get(this.guildId)!
+  }
+
+  public getChannel (): StageChannel {
+    const guild = this.getGuild()
+    return guild.getChannels().getCache().get(this.guildId)!
+  }
+
+  public getTopic (): string {
+    return this.topic
+  }
+
+  public getPrivacyLevel (): number {
+    return this.privacyLevel
+  }
+
+  public isDiscoverableDisabled (): boolean {
+    return this.discoverableDisabled
   }
 }
