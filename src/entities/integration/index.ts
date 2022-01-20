a12fce05a1dab42ce1f1e906586db297f0bf2f56
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:44:41 +0100

    :construction: Create getters

--- src/entities/integration/index.ts
+++ src/entities/integration/index.ts
@@ -6,22 +6,81 @@ import IntegrationApplication from './IntegrationApplication'
 
 export default class Integration {
   constructor (
-    public id: Snowflake,
-    public name: string,
-    public type: 'twitch' | 'youtube' | 'discord',
-    public isEnabled: boolean,
-    public isSyncing: boolean,
-    public roleId: Snowflake,
-    public isEnableEmoticons: boolean,
-    public expireBehavior: BehaviorsExpiration,
-    public expireGracePeriod: number,
-    public user: User,
-    public account: IntegrationAccount,
-    public syncedAt: DateTime,
-    public subscriberCount: number,
-    public isRevoked: boolean,
-    public application: IntegrationApplication,
-
+    protected id: Snowflake,
+    protected name: string,
+    protected type: 'twitch' | 'youtube' | 'discord',
+    protected enabled: boolean,
+    protected syncing: boolean,
+    protected roleId: Snowflake,
+    protected enableEmoticons: boolean,
+    protected expireBehavior: BehaviorsExpiration,
+    protected expireGracePeriod: number,
+    protected user: User,
+    protected account: IntegrationAccount,
+    protected syncedAt: DateTime,
+    protected subscriberCount: number,
+    protected revoked: boolean,
+    protected application: IntegrationApplication,
   ) {
+  }
+
+  public getId (): Snowflake {
+    return this.id
+  }
+
+  public getName (): string {
+    return this.name
+  }
+
+  public getType (): 'twitch' | 'youtube' | 'discord' {
+    return this.type
+  }
+
+  public isEnabled (): boolean {
+    return this.enabled
+  }
+
+  public isSyncing (): boolean {
+    return this.syncing
+  }
+
+  public getRoleId (): Snowflake {
+    return this.roleId
+  }
+
+  public isEnableEmoticons (): boolean {
+    return this.enableEmoticons
+  }
+
+  public getExpireBehavior (): BehaviorsExpiration {
+    return this.expireBehavior
+  }
+
+  public getExpireGracePeriod (): number {
+    return this.expireGracePeriod
+  }
+
+  public getUser (): User {
+    return this.user
+  }
+
+  public getAccount (): IntegrationAccount {
+    return this.account
+  }
+
+  public getSyncedDate (): DateTime {
+    return this.syncedAt
+  }
+
+  public getSubscriberCount (): number {
+    return this.subscriberCount
+  }
+
+  public isRevoked (): boolean {
+    return this.revoked
+  }
+
+  public getApplication (): IntegrationApplication {
+    return this.application
   }
 }
