ec2b087ea650caf26496cdf29da0b9741ba0896a
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:39:41 +0100

    :construction: Create getters

--- src/entities/guild/GuildHashes.ts
+++ src/entities/guild/GuildHashes.ts
@@ -2,9 +2,21 @@ import { Hash } from '../../types'
 
 export default class GuildHashes {
   constructor (
-    public roles: Hash,
-    public metadata: Hash,
-    public channels: Hash,
+    private roles: Hash,
+    private metadata: Hash,
+    private channels: Hash,
   ) {
+  }
+
+  public getRoles (): Hash {
+    return this.roles
+  }
+
+  public getMetadata (): Hash {
+    return this.metadata
+  }
+
+  public getChannels (): Hash {
+    return this.channels
   }
 }
