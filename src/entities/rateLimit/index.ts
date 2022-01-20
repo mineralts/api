935572d60ad3cefce04c80848e43ea3886329f0e
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:45:02 +0100

    :construction: Create getters

--- src/entities/rateLimit/index.ts
+++ src/entities/rateLimit/index.ts
@@ -2,9 +2,21 @@ import { DateTime } from 'luxon'
 
 export default class RateLimit {
   constructor (
-    public message: string,
-    public retryAfter: DateTime,
-    public isGlobal: boolean,
+    private message: string,
+    private retryAfter: DateTime,
+    private global: boolean,
   ) {
+  }
+
+  public getMessage (): string {
+    return this.message
+  }
+
+  public getTimeout (): DateTime {
+    return this.retryAfter
+  }
+
+  public isGlobal (): boolean {
+    return this.global
   }
 }
