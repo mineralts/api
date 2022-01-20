86443dee8932446999036c01ffc7679b9065eb7a
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-01-20 08:45:06 +0100

    :construction: Create getters

--- src/entities/roles/index.ts
+++ src/entities/roles/index.ts
@@ -3,17 +3,61 @@ import Guild from '../guild/Guild'
 
 export default class Role {
   constructor (
-    public id: Snowflake,
-    public name: string,
-    public unicodeEmoji: string | null,
-    public position: number,
-    public permissions: string,
-    public mentionable: boolean,
-    public managed: boolean,
-    public icon: any,
-    public hoist: boolean,
-    public color: number,
-    public guild: Guild
+    private id: Snowflake,
+    private name: string,
+    private unicodeEmoji: string | null,
+    private position: number,
+    private permissions: string,
+    private mentionable: boolean,
+    private managed: boolean,
+    private icon: any,
+    private hoist: boolean,
+    private color: number,
+    private guild: Guild
   ) {
+  }
+
+  public getId (): Snowflake {
+    return this.id
+  }
+
+  public getName (): string {
+    return this.name
+  }
+
+  public getUnicodeEmoji (): string | null {
+    return this.unicodeEmoji
+  }
+
+  public getPosition (): number {
+    return this.position
+  }
+
+  public getPermissions (): string {
+    return this.permissions
+  }
+
+  public isMentionable (): boolean {
+    return this.mentionable
+  }
+
+  public isManaged (): boolean {
+    return this.managed
+  }
+
+  public getIcon (): string {
+    return this.icon
+  }
+
+  public getHoist (): boolean {
+    return this.hoist
+  }
+
+  public getColor (): number {
+    return this.color
+  }
+
+  public getGuild (): Guild {
+    return this.guild
   }
 }
